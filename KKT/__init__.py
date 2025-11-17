ac74713d372152d8e0366d9dcde9ee5adef99767
Serhiy Kapustyan <serhiy.kapustyan@gmail.com>
2025-11-17 21:38:11 +0100

    delete init

--- KKT/__init__.py
+++
@@ -1,6 +0,0 @@
-"""KKT (Karush-Kuhn-Tucker) condition utilities.
-
-This package provides tools for forming and displaying KKT optimality
-conditions for linear programs (LP), quadratic programs (QP), and
-nonlinear programs (NLP).
-"""
